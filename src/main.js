ad6347f9c563e693b14669ad35a7ec829781537a
remus <remus.poienar.not@gmail.com>
2018-10-25 22:56:43 +0300

    .

---
+++ src/main.js
@@ -0,0 +1,13 @@
+import Vue from "vue";
+import VeeValidate from "vee-validate";
+
+import App from "./App.vue";
+import router from "./router";
+
+Vue.use(VeeValidate);
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount("#app");
